8f3f633d05ed72dce9710bf6714b8dda04b5119d
manoahLinks <manoahluka@gmail.com>
2023-08-13 10:56:18 +0100

    config for cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
 import Africastalking from 'africastalking'
-import express from 'express'     
+import express from 'express'
+import cors from 'cors'     
     
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
