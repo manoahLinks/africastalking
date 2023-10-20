72e3293967ec951c53f593f9293f5f50392dd823
Oshioke-Salaki <salaki1902@gmail.com>
2023-10-20 17:03:52 -0600

    fixed medications component and medication order

--- client/src/components/MedicationForm.jsx
+++ client/src/components/MedicationForm.jsx
@@ -13,20 +13,14 @@ function MedicationForm() {
   }
   return (
     <div className="flex flex-col gap-y-4">
-      <button
-        className="text-base self-center p-2 rounded bg-[#c4c4c4] text-white"
-        onClick={handleAdd}
-      >
-        add
-      </button>
       <h4 className="text-center font-semibold">Medication Order</h4>
-      <div className="grid grid-cols-[377px_203px] gap-x-[24px]">
+      <div className="grid grid-cols-[300px_203px_auto] gap-x-[24px]">
         <div className="flex flex-col gap-y-2">
           <small>Medicine</small>
           <input
             type="text"
             name="medicine"
-            className="text-xs placeholder-slate-300 border w-[377px] border-slate-300 rounded-md"
+            className="text-xs placeholder-slate-300 border w-[300px] border-slate-300 rounded-md"
             placeholder="Enter drug name"
             value={medcine}
             onChange={(e) => setMedcine(e.target.value)}
@@ -44,17 +38,23 @@ function MedicationForm() {
             onChange={(e) => setDosage(e.target.value)}
           />
         </div>
+        <button
+          className="text-base self-center p-2 rounded bg-[#c4c4c4] text-white"
+          onClick={handleAdd}
+        >
+          add
+        </button>
       </div>
       {orderList.length !== 0 && (
         <div>
-          <div className="grid grid-cols-[377px_203px] gap-x-[24px] text-black  text-base font-normal">
+          <div className="grid grid-cols-[300px_203px_auto] gap-x-[24px] text-black  text-base font-normal">
             <h2>Drugs</h2> <h2>Dosage</h2>
           </div>
           <div className="flex flex-col gap-y-2">
             {orderList.map((order, i) => (
               <div
                 key={i}
-                className="grid grid-cols-[377px_203px] gap-x-[24px] text-blackLight300 text-base font-light"
+                className="grid grid-cols-[300px_auto] gap-x-[24px] text-blackLight300 text-base font-light"
               >
                 <h2>{order.medcine}</h2>
                 <h2>{order.dosage}</h2>
